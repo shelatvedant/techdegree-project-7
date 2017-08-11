861daddb8a20160d6ac2e8bddc2ee452b0148aab
Shelat Vedant <shelatvedant@gmail.com>
2017-08-11 13:16:50 +0200

    Adding JS

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/* variables */
+
+const video = document.getElementById('video');
+const transcriptDiv = document.querySelector('.transcript');
+const spanText = document.querySelectorAll('.transcript span');
+
+/* As video plays, the transcript highlights*/
+
+video.addEventListener('timeupdate', () => {
+  for(let i = 0; i < spanText.length; i += 1){
+    let videoTime = video.getCurrentTime();
+    let startVideoTime = spanText[i].getAttribute('data-start');
+    let endVideoTime = spanText[i].getAttribute('data-end');
+
+    if(videoTime >= startVideoTime && videoTime <= endVideoTime){
+      spanText[i].className = 'highlight';
+    }else {
+      spanText[i].className = '';
+    }
+  }
+});
